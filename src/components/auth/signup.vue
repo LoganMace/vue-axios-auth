e9dac2eb318411338b1fe63212026d5a97e90a8d
Logan Mace <lmmace92@hotmail.com>
2018-10-21 15:39:27 -0500

    added email validation and styling

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -2,12 +2,15 @@
   <div id="signup">
     <div class="signup-form">
       <form @submit.prevent="onSubmit">
-        <div class="input">
+        <div class="input" :class="{ invalid: $v.email.$error }">
           <label for="email">Mail</label>
           <input
                   type="email"
                   id="email"
+                  @input="$v.email.$touch()"
                   v-model="email">
+          <p v-if="!$v.email.email">Please provide a valid email address.</p>
+          <p v-if="!$v.email.required">Must not be empty.</p>
         </div>
         <div class="input">
           <label for="age">Your Age</label>
@@ -69,6 +72,7 @@
 </template>
 
 <script>
+  import { required, email } from 'vuelidate/lib/validators';
 
   export default {
     data () {
@@ -80,6 +84,12 @@
         country: 'usa',
         hobbyInputs: [],
         terms: false
+      }
+    },
+    validations: {
+      email: {
+        required,
+        email
       }
     },
     methods: {
@@ -149,6 +159,15 @@
     outline: none;
     border: 1px solid #521751;
     background-color: #eee;
+  }
+
+  .input.invalid input {
+    border: 1px solid red;
+    background-color: #ffc9aa;
+  }
+
+  .input.invalid label {
+    color: red;
   }
 
   .input select {
